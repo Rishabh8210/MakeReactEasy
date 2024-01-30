9987c6afd22e5f9918c09f74436f62523d6c9f12
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-01-30 09:10:18 +0530

    Updated the Footer, added restaurant location

--- Learning React Day 5/Assignment 5 Coding/src/components/Footer.js
+++ Learning React Day 5/Assignment 5 Coding/src/components/Footer.js
@@ -1,4 +1,24 @@
+import Footer_IMG from "../Assets/images/foodify.png"
 const Footer = ()=>{
-    return <h1>Footer</h1>
+    return (
+        <div className="footer-screen">
+            <div className="footer-left-card">
+                <img src={Footer_IMG} />
+                <h3>Our cooking is Best, Our maintanence is Best, Our Delivery is BEST !</h3>
+            </div>
+            <div className="footer-restaturant-city-list">
+                <p>Ranchi</p>
+                <p>Patna</p>
+                <p>Mumbai</p>
+                <p>Jammu</p>
+                
+                <p>Mumbai</p>
+                <p>Jammu</p>
+                <p>Jamui</p>
+                <p>Vellore</p>
+                <p>Vellore</p>
+            </div>
+        </div>
+    )
 }
 export default Footer;
